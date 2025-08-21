eb3ca4205892631c35b050e38a81186590502bad
Robert-Vaduva <36301528+eviLMonkeyLive@users.noreply.github.com>
2025-08-21 13:09:50 +0200

    Added environment variables to hide sensitive data

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,34 +1,27 @@
+"""
+This module provides functionality to retrieve animal data from an external API.
+
+It defines a function `fetch_data(animal_name)` which queries the API using the
+given animal name and returns a list of matching animals in JSON format.
+"""
+
+
+import os
 import requests
+from dotenv import load_dotenv
 
 
+# Load environment variables from .env
+load_dotenv()
 API_URL = 'https://api.api-ninjas.com/v1/animals?name='
-API_KEY = "IvLPDV+GnuYbo+XJr2VjeA==Saj417C9U3BISPBz"
+API_KEY = os.getenv('API_KEY')
 
 
 def fetch_data(animal_name):
-    """
-    Fetches the animals data for the animal 'animal_name'.
-    Returns: a list of animals, each animal is a dictionary:
-    {
-    'name': ...,
-    'taxonomy': {
-      ...
-        },
-    'locations': [
-      ...
-        ],
-    'characteristics': {
-      ...
-        }
-    },
-    """
+    """Fetch a list of animals from the API matching the given animal name."""
     list_of_animals = []
     response = requests.get(API_URL + animal_name, headers={'X-Api-Key': f'{API_KEY}'})
     animals = response.json()
     for animal in animals:
         list_of_animals.append(animal)
     return list_of_animals
-
-
-if __name__ == "__main__":
-    print(fetch_data(str(input("Enter a name of an animal: "))))
